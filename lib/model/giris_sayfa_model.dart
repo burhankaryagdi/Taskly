18794dd7318832b7af794a6d8a2b237d0ea71088
Mehmet Gençdal <92645933+mehmetgencdal@users.noreply.github.com>
2024-12-26 10:33:25 +0300

    Update giris_sayfa_model.dart

--- lib/model/giris_sayfa_model.dart
+++ lib/model/giris_sayfa_model.dart
@@ -1,3 +1,6 @@
+// Flutter'da model isimleri PascalCase olmalı ve her modelimiz bir nesne olacağı için isimlendirmesi de bu şekilde olmalıdır. Örnek: SaydamPerncere, Araba, Kullanici, TaksiSoforu vb
+// Zorunlu olmasa da toJson ve fromJson metodlarını modellere eklemek önemlidir.
+
 class saydamPencereKontrol{
   String baslik;
   String aciklama;

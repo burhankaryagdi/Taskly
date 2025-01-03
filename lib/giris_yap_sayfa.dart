30d4f8938b10eb3b9438178c06afb255854e1a22
Burhan KARYAĞDI <151432308+Castlexe@users.noreply.github.com>
2025-01-03 10:07:29 +0300

    APİ ile Geçici Giriş Adımı
    Geçici olarak örnek bir APİ ile giriş yapabilme eklendi daha sonra tekrar düzenlenecek.

--- lib/giris_yap_sayfa.dart
+++ lib/giris_yap_sayfa.dart
@@ -1,8 +1,21 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:taskly/ana_sayfa.dart';
+import 'package:http/http.dart' as http;
 
-class GirisYap extends StatelessWidget {
+class GirisYap extends StatefulWidget {
   const GirisYap({super.key});
+
+  @override
+  State<GirisYap> createState() => _GirisYapState();
+}
+
+class _GirisYapState extends State<GirisYap> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  bool isLoading =false;
+  String errorMessage ="";
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +44,7 @@ class GirisYap extends StatelessWidget {
                 height: 10,
               ),
               TextField(
+                controller: _emailController,
                 decoration: InputDecoration(
                     labelText: "Kullanıcı Adı",
                     border: OutlineInputBorder(
@@ -40,6 +54,7 @@ class GirisYap extends StatelessWidget {
                 height: 10,
               ),
               TextField(
+                controller: _passwordController,
                 obscureText: true,
                 decoration: InputDecoration(
                     labelText: "Şifre",
@@ -49,15 +64,20 @@ class GirisYap extends StatelessWidget {
               SizedBox(
                 height: 10,
               ),
+              if(errorMessage.isNotEmpty)
+                Text(
+                  errorMessage,style: TextStyle(color: Colors.red),
+                ),
+              isLoading ? CircularProgressIndicator() :
               ElevatedButton(
                 child: Text(
                   "Giriş Yap",
                   style: TextStyle(fontSize: 25, color: Colors.blue[800]),
                 ),
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AnaSayfa()),
+                  login(
+                    _emailController.text.trim(),
+                    _passwordController.text.trim()
                   );
                 },
               )
@@ -66,5 +86,44 @@ class GirisYap extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void login(String email, String password) async {
+    final url = Uri.parse("https://dummyjson.com/users");
+    try {
+      final response = await http.get(url); //kullanıcı listesini getiriyor.
+
+      if (response.statusCode == 200) {
+        final List users = jsonDecode(response.body)["users"];
+        final user = users.firstWhere(
+          (user) => user["email"] == email && user["password"] == password,
+          orElse: () => null,
+        );
+        if (user != null) { //Giriş Başarılı ise;
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AnaSayfa()));
+        } else {
+          // Kullanıcı Bulunamadi ise;
+          setState(() {
+            errorMessage ="Geçersiz E-posta veya şifre!";
+          });
+        }
+      }
+      else{
+        //Api hatalı
+        setState(() {
+          errorMessage = "Sunucu Hatası ${response.statusCode}";
+        });
+      }
+    } catch (e) {
+      //Ağ Bağlantısı hatası
+      setState(() {
+        errorMessage ="Bağlantı Hatası: $e";
+      });
+    }finally{
+      setState(() {
+        isLoading = false; //göstergeyi kapat
+      });
+    }
   }
 }
